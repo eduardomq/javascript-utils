829885e64d41babc336e80788de36d6be3a24389
Eduardo MQ <34769842+eduardomq@users.noreply.github.com>
2021-11-12 13:23:52 +0100

    Fix

--- coordinates.ts
+++ coordinates.ts
@@ -1,7 +1,7 @@
 export function generateRandomCoordsByBounds(bounds: {minLon, maxLon, minLat, maxLat}, elements: number, precision = 3): number[][] {
     const points = [];
 
-    for (let i = 0; i <= elements; i++) {
+    for (let i = 0; i < elements; i++) {
         let lat = Math.random() * (bounds.maxLat - bounds.minLat) + bounds.minLat;
         let lon = Math.random() * (bounds.maxLon - bounds.minLon) + bounds.minLon;
         lat = +lat.toFixed(precision);
